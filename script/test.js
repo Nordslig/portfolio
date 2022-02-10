336ea6bd1af74ddbbacaac763d4a35d29d3f576a
Bartosz Pindel <kaladin112@gmail.com>
2022-02-10 16:43:01 +0100

    add temporarily  test.js, change submit button, code format

---
+++ script/test.js
@@ -0,0 +1,9 @@
+// console.log(document.querySelector('.submit'));
+const currentTime = new Date();
+
+const messageSendTime = currentTime.getHours() + ':' + currentTime.getMinutes();
+
+document.querySelector('.submit').addEventListener('click', (e) =>{
+  e.preventDefault();
+  console.log(`Wysłano wiadomość o godzinie ${messageSendTime}`);
+})
