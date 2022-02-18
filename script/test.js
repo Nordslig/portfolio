b2d1a20fdd7eeb7d034f4b00e6d13c1a2ea315d6
Bartosz Pindel <kaladin112@gmail.com>
2022-02-18 11:10:23 +0100

    chagne test.js to get more information

--- script/test.js
+++ script/test.js
@@ -4,5 +4,11 @@ const messageSendTime = currentTime.getHours() + ':' + currentTime.getMinutes();
 
 document.querySelector('.submit').addEventListener('click', (e) =>{
   e.preventDefault();
-  console.log(`Wysłano wiadomość o godzinie ${messageSendTime}`);
+  
+  const senderName = document.querySelector('.sender').value;
+
+  console.log(`Wiadomość wysłana o godzinie ${messageSendTime}, przez użytkownika ${senderName}`)
+
+  // alert(`Wiadomość wysłana o godzinie ${messageSendTime}, przez użytkownika ${senderName}`)
+
 })
