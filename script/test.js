84c85818c9c020407b79757c6a4d5b3024b21904
Bartosz Pindel <kaladin112@gmail.com>
2022-02-19 13:51:29 +0100

    small change in skills, projects, form

--- script/test.js
+++ script/test.js
@@ -6,8 +6,19 @@ document.querySelector('.submit').addEventListener('click', (e) =>{
   e.preventDefault();
   
   const senderName = document.querySelector('.sender').value;
-
-  console.log(`Wiadomość wysłana o godzinie ${messageSendTime}, przez użytkownika ${senderName}`)
+  const senderMail = document.querySelector('.mail').value;
+  // const senderName = document.querySelector('.sender').value;
+  
+  if(!senderName && !senderMail){
+    alert(`Uzupełnij wszystkie pola`)
+  } else if(!senderMail){
+    alert(`Uzupełnij email`)
+  }else if (!senderName){
+    alert(`Uzupełnij imię`)
+  }
+  else{
+    console.log(`Wiadomość wysłana o godzinie ${messageSendTime}, przez użytkownika ${senderName}`)
+  }
 
   // alert(`Wiadomość wysłana o godzinie ${messageSendTime}, przez użytkownika ${senderName}`)
 
