5baaf7d4317d814485549908465d39b310455796
Bartosz Pindel <kaladin112@gmail.com>
2022-04-04 13:23:46 +0200

    add feature to projects section

--- script/nextProject.js
+++ script/nextProject.js
@@ -2,19 +2,28 @@ const projects = document.querySelectorAll('.project');
 const projectsArr = [...projects];
 const projectsTitlesArr = ['Weather API', 'Mini funkcja', 'Portfolio'];
 
+const projectsDots = document.querySelectorAll('.dot');
+const projectsDotsArr = [...projectsDots];
+
 const nextProjectTitle = document.querySelector('.nextProjectTitle');
 
 nextProjectTitle.textContent = projectsTitlesArr[0];
 
 document.querySelector('button.arrow').addEventListener('click', () => {
   projectsArr[0].classList.toggle('visible');
-  const visPro = projectsArr.shift();
-  projectsArr.push(visPro);
+  const visibleProject = projectsArr.shift();
+  projectsArr.push(visibleProject);
   projectsArr[0].classList.toggle('visible');
   
   const nextProName = projectsTitlesArr.shift();
   projectsTitlesArr.push(nextProName);
 
   nextProjectTitle.textContent = projectsTitlesArr[0];
+
+  projectsDotsArr[0].classList.toggle('active');
+  const activeDot = projectsDotsArr.shift();
+  projectsDotsArr.push(activeDot);
+  projectsDotsArr[0].classList.toggle('active');
+  
 
 });
