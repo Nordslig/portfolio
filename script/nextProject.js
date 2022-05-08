03c38b4e4ec2fa569b051aab9e480925b3b2823d
Bartosz Pindel <kaladin112@gmail.com>
2022-05-08 14:34:33 +0200

    add small fixes, end project 5/8/22

--- script/nextProject.js
+++ script/nextProject.js
@@ -1,29 +1,19 @@
-const projects = document.querySelectorAll('.project');
+const projects = document.querySelectorAll(".project");
 const projectsArr = [...projects];
-const projectsTitlesArr = ['Weather API', 'Mini funkcja', 'Portfolio'];
+const projectsTitlesArr = ["Weather API", "Mini funkcja", "Portfolio"];
 
-const projectsDots = document.querySelectorAll('.dot');
-const projectsDotsArr = [...projectsDots];
-
-const nextProjectTitle = document.querySelector('.nextProjectTitle');
+const nextProjectTitle = document.querySelector(".nextProjectTitle");
 
 nextProjectTitle.textContent = projectsTitlesArr[0];
 
-document.querySelector('button.arrow').addEventListener('click', () => {
-  projectsArr[0].classList.toggle('visible');
+document.querySelector("button.arrow").addEventListener("click", () => {
+  projectsArr[0].classList.toggle("visible");
   const visibleProject = projectsArr.shift();
   projectsArr.push(visibleProject);
-  projectsArr[0].classList.toggle('visible');
-  
+  projectsArr[0].classList.toggle("visible");
+
   const nextProName = projectsTitlesArr.shift();
   projectsTitlesArr.push(nextProName);
 
   nextProjectTitle.textContent = projectsTitlesArr[0];
-
-  projectsDotsArr[0].classList.toggle('active');
-  const activeDot = projectsDotsArr.shift();
-  projectsDotsArr.push(activeDot);
-  projectsDotsArr[0].classList.toggle('active');
-  
-
 });
